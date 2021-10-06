f1f644562dcb304515ac9a2578c06f3567e33191
siavash sattari <siavash.sattari.75@gmail.com>
2021-10-06 22:30:11 +0330

    Search City

--- js/app.js
+++ js/app.js
@@ -1,10 +1,35 @@
 let weatherData = new Weather("tehran");
 let ui = new UI();
 
-weatherData
-  .getWeather()
-  .then((data) => {
-    console.log(data);
-    ui.setWeatherInfo(data);
-  })
-  .catch((err) => console.log(err));
+// Load Content
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  getWeather();
+});
+
+// Change The City
+
+document.querySelector("#save-change").addEventListener("click", changeCity);
+
+// Functions
+
+function getWeather() {
+  weatherData
+    .getWeather()
+    .then((data) => {
+      console.log(data);
+      ui.setWeatherInfo(data);
+      document.querySelector("#city").value = "";
+    })
+    .catch((err) => {
+      alert("Please serach for a valid city!");
+      document.querySelector("#city").value = "";
+    });
+}
+
+function changeCity() {
+  let city = document.querySelector("#city").value;
+  weatherData.chageCity(city);
+  getWeather();
+  $("#showModals").modal("hide");
+}
